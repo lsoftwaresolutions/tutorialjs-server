a7cabff649242877a7ea137b1ae6b8c5f66c2811
Ruslan Lekhman <ruslan@intellimedia.ca>
2016-12-05 05:17:06 +0200

    Init

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,34 @@
+**Note: for support questions, please use one of these channels:** [Twitter: @lekhmanrus](https://twitter.com/lekhmanrus)
+
+* **I'm submitting a ...**
+[ ] bug report
+[ ] feature request
+[ ] question about the decisions made in the repository
+
+* **Do you want to request a *feature* or report a *bug*?**
+
+
+
+* **What is the current behavior?**
+
+
+
+* **If the current behavior is a bug, please provide the steps to reproduce and if possible a minimal demo of the problem**
+
+
+
+* **What is the expected behavior?**
+
+
+
+* **What is the motivation / use case for changing the behavior?**
+
+
+
+* **Please tell us about your environment:**
+
+- Node.js version: X.Y.Z
+
+
+
+* **Other information** (e.g. detailed explanation, stacktraces, related issues, suggestions how to fix, links for us to have context, eg. stackoverflow, gitter, etc)
---
+++ src/api/user/model.js
@@ -0,0 +1,137 @@
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+import randtoken from 'rand-token'
+import mongoose, { Schema } from 'mongoose'
+import mongooseKeywords from 'mongoose-keywords'
+import { env } from '../../config'
+
+const compare = require('bluebird').promisify(bcrypt.compare)
+const roles = ['user', 'admin']
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    match: /^\S+@\S+\.\S+$/,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  login: {
+    type: String,
+    index: true,
+    trim: true
+  },
+  firstname: {
+    type: String,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    trim: true
+  },
+  birthday: {
+    type: Date
+  },
+  levelId: {
+    type: Number
+  },
+  points: {
+    type: Number,
+    default: 0
+  },
+  reputation: {
+    type: Number,
+    default: 0
+  },
+  services: {
+    facebook: String,
+    github: String,
+    google: String
+  },
+  role: {
+    type: String,
+    enum: roles,
+    default: 'user'
+  },
+  picture: {
+    type: String,
+    trim: true
+  }
+}, {
+  timestamps: true
+})
+
+userSchema.path('email').set(function (email) {
+  if (!this.picture || this.picture.indexOf('https://gravatar.com') === 0) {
+    const hash = crypto.createHash('md5').update(email).digest('hex')
+    this.picture = `https://gravatar.com/avatar/${hash}?d=identicon`
+  }
+
+  if (!this.login) {
+    this.login = email.replace(/^(.+)@.+$/, '$1')
+  }
+
+  return email
+})
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
+
+  /* istanbul ignore next */
+  const rounds = env === 'test' ? 1 : 9
+
+  bcrypt.hash(this.password, rounds, (err, hash) => {
+    /* istanbul ignore next */
+    if (err) return next(err)
+    this.password = hash
+    next()
+  })
+})
+
+userSchema.methods = {
+  view (full) {
+    let view = {}
+    let fields = ['id', 'login', 'picture']
+
+    if (full) {
+      fields = [...fields, 'email', 'createdAt', 'updatedAt']
+    }
+
+    fields.forEach((field) => { view[field] = this[field] })
+
+    return view
+  },
+
+  authenticate (password) {
+    return compare(password, this.password).then((valid) => valid ? this : false)
+  }
+}
+
+userSchema.statics = {
+  roles,
+
+  createFromService ({ service, id, email, login, picture }) {
+    return this.findOne({ $or: [{ [`services.${service}`]: id }, { email }] }).then((user) => {
+      if (user) {
+        user.services[service] = id
+        user.login = login
+        user.picture = picture
+        return user.save()
+      } else {
+        const password = randtoken.generate(16)
+        return this.create({ services: { [service]: id }, email, password, login, picture })
+      }
+    })
+  }
+}
+
+userSchema.plugin(mongooseKeywords, { paths: ['email', 'login'] })
+
+module.exports = mongoose.model('User', userSchema)
+export default module.exports
